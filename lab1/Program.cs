4310da8477def3c567fc93b973a1f2356e19d7af
Yuliia <yuliiashurek@gmail.com>
2023-09-30 13:32:19 +0300

    practiceTutorial mistake fixed (in new project lab1)

---
+++ lab1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace lab1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            MyNewList<int> list = new MyNewList<int>(5) { 1, 2, 3, 4, 5 };
+            foreach (var el in list)
+            {
+                Console.WriteLine(el);
+            }
+        }
+    }
+}
