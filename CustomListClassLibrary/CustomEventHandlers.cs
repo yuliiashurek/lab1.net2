176f7f5677f1a42e88cc3b727a056d90547179cf
Yuliia <yuliiashurek@gmail.com>
2023-10-16 20:29:19 +0300

    event tests added

--- CustomListClassLibrary/CustomEventHandlers.cs
+++ CustomListClassLibrary/CustomEventHandlers.cs
@@ -11,11 +11,11 @@ public static class CustomEventHandlers
 
     public static void PrintListEventHandler(object sender, CustomListBaseEventArgs e)
     {
-        Console.WriteLine($"\t\t\t\t\t\t\t\t\tEvent: {e.ModificationTypes} {e.DateTime}");
+        Console.WriteLine($"\t\t\t\t\t\t\t\t\tEvent: {e.ModificationTypes} Date: {e.DateTime}");
     }
         
     public static void PrintListResizedEventHandler(object sender, CustomListEventArgs e)
     {
-        Console.WriteLine($"\t\t\t\t\t\t\t\t\tEvent: {e.ModificationTypes} Old capacity: {e.OldCapacity} New capacity: {e.NewCapacity} {e.DateTime}");
+        Console.WriteLine($"\t\t\t\t\t\t\t\t\tEvent: {e.ModificationTypes} Old capacity: {e.OldCapacity} New capacity: {e.NewCapacity} Date: {e.DateTime}");
     }
 }
